2d902f45268d79aa22ff48da7685beffde52212f
chrisstarrh <104403611+chrisstarrh@users.noreply.github.com>
2026-05-21 12:58:57 -0700

    fix: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   experimental: {
     serverComponentsExternalPackages: ['@anthropic-ai/sdk'],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
-
 module.exports = nextConfig;
