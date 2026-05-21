c59fd45c5e3616235fd4fde56e2aad6aab2042f5
chrisstarrh <104403611+chrisstarrh@users.noreply.github.com>
2026-05-21 07:37:16 -0700

    fix: lib/supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,2 +1,7 @@
-import{createClientComponentClient}from'@supabase/auth-helpers-nextjs';
-export const createBrowserClient=()=>createClientComponentClient();
+import { createClient } from '@supabase/supabase-js';
+
+export const createBrowserClient = () =>
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
